d4e35acb1af8b14b59c72b84300bc939e4e9d69f
Gerrit Tielens <gerrit.tielens@outlook.com>
2012-08-24 20:38:27 +0200

    Complete tutorial, minor modifs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :body, :commenter
+  
+  #validates :commenter, :presence => true
+  #validates :body, :length => { :minimum => 5 }
 end
